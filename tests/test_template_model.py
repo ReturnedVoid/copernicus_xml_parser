66e23d5b5115829a6dbada49a7ded764338eb5e3
ReturnedVoid <andrewnech@gmail.com>
2018-04-04 14:12:02 +0300

    tested template_model module

--- tests/test_template_model.py
+++ tests/test_template_model.py
@@ -1,10 +1,32 @@
-from sample import template_model
+from sample.template_model import *
+from sample.parser import *
+from sample.issue_model import *
 
 
 def test_get_doc_with_valid_args():
-    parser = template_model.Parser()
-    assert parser.get_doc_text(template_model.ESimpleType.roleType, template_model.EEnumeration.AUTHOR) \
+    parser = Parser()
+    assert parser.get_doc_text(ESimpleType.roleType, EEnumeration.AUTHOR) \
            == 'Author (including co-author) / Autor (w tym Współautor)'
 
 
-test_get_doc_with_valid_args()
+def test_get_doc_after_set():
+    test_text = 'Test'
+    parser = Parser()
+    parser.set_doc_text(ESimpleType.roleType, EEnumeration.AUTHOR, test_text)
+    text = parser.get_doc_text(ESimpleType.roleType, EEnumeration.AUTHOR)
+    assert text == test_text
+
+
+def test_get_element_annotation():
+    parser = Parser()
+    expected = 'Schemat importu danych'
+    assert parser.get_element_annotation() == expected
+
+
+def test_insertion():
+    expected = 'Kherson National Technical University'
+    fill_template_xml(ISSUE_FILE)
+
+    parser = Parser('outputs/output_num0.xsd')
+    text = parser.get_doc_text(EElement.TYPE)
+    assert text == expected
