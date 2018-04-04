0fd6d4cdf695d3ec1f134825217572776ddf4303
ReturnedVoid <andrewnech@gmail.com>
2018-04-04 16:45:46 +0300

    renamed file

--- src/copernicus_parser.py
+++ src/copernicus_parser.py
@@ -1,10 +1,9 @@
-from sample.template_model import Parser, EEnumeration, ESimpleType, EElement, TEMP_FILE_NAME
-from sample.issue_model import Root
+from template_model import Parser, EElement, TEMP_FILE_NAME
+from issue_model import Root
 import os
-import sys
-from sample.issue_model import XML_FILE_NAME as ISSUE_FILE
+from issue_model import XML_FILE_NAME as ISSUE_FILE
 
-OUTPUT_DIR_NAME = '../outputs'
+OUTPUT_DIR_NAME = 'outputs'
 
 
 def fill_template_xml(xml_file):
@@ -41,7 +40,7 @@ def fill_template_xml(xml_file):
                     affiliation = author.get_eng_affiliation()
                     parser.set_doc_text(EElement.TYPE, text=affiliation)
 
-            file_name = '../outputs/output_num{}.xsd'.format(j)
+            file_name = 'outputs/output_num{}.xsd'.format(j)
             parser.write_xml_to_file(file_name)
 
     # Delete unnecessary temp file
